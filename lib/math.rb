b1800cf7dc126ea76fe3d39f77b4781c06a93dea
Maasa Kono <konomaasa@gmail.com>
2019-04-19 23:06:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,21 @@
 def addition(num1, num2)
-  1+1
+  1 + 1
 end
 
 def subtraction(num1, num2)
-  3-2
+  3 - 2
 end
 
 def division(num1, num2)
-  4/2
+  4 / 2
 end
 
 def multiplication(num1, num2)
-  2*2
+  2 * 2
 end
 
 def modulo(num1, num2)
-  9%5
+  9 % 5
 end
 
 def square_root(num)
@@ -23,6 +23,6 @@ def square_root(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  1 + ((2*2)/2)
+  1 + ((2 * 2) /2)
   #Hint:  __ + (( __ * __ ) / __ )
 end
