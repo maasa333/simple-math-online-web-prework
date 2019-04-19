7e09bad5414b0d11b94dd21aa8d7161f83ecac19
Maasa Kono <konomaasa@gmail.com>
2019-04-19 23:08:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,10 +19,10 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  Math.sqrt(16)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  1 + ((2 * 2) /2)
+  7 + ((43*23) / 83)
   #Hint:  __ + (( __ * __ ) / __ )
 end
