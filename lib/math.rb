d0ca8d250a43bb83764177d90f638bdb428486e8
Maasa Kono <konomaasa@gmail.com>
2019-04-19 23:03:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,17 @@
 def addition(num1, num2)
+  1+1
 end
 
 def subtraction(num1, num2)
+  3-2
 end
 
 def division(num1, num2)
+  4/2
 end
 
 def multiplication(num1, num2)
+  2*2
 end
 
 def modulo(num1, num2)
